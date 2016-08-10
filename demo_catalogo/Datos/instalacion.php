c528124947106cbd003c8efa23cf39701fcc8d03
Ricardo_2 <m_ricky3021@hotmail.com>
2016-08-10 09:41:24 -0500

    demo

---
+++ demo_catalogo/Datos/instalacion.php
@@ -0,0 +1,20 @@
+<?php
+	
+	include_once('conexion.php');
+
+	function crearBD(){
+		$con= new conexion();
+		$cn= $con->conectar();
+
+		$sql="CREATE DATABASE demo_catalogo";
+		if(mysqli_query($cn,$sql))
+		{
+			echo "bd creada";
+		}else{
+			echo "error o BD ya esta creada";
+		}
+
+		mysqli_close($cn);
+	}
+	crearBD();
+?>
